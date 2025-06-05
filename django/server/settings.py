1662eedce6a39fe35835330fc320c0f6a489c312
Jiaxu <100826140+Cai-Jiaxu@users.noreply.github.com>
2025-06-05 06:48:28 +0000

    update settings.py to use supabase

--- django/server/settings.py
+++ django/server/settings.py
@@ -1,9 +1,11 @@
 # server/settings.py
 from pathlib import Path
+import dj_database_url
+import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-import os
+
 
 SECRET_KEY = os.getenv('SECRET_KEY', 'unsafe-dev-secret')
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
@@ -53,11 +55,17 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "server.wsgi.application"
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.config(
+        default=os.getenv("DATABASE_URL")
+    )
 }
 
 AUTH_PASSWORD_VALIDATORS = [
